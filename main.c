77a5c821c1cc0f975db8e16e49c7c0a6c27d9221
ixxeh_p <51870454+ixxeh-p@users.noreply.github.com>
2024-06-11 11:48:11 +0900

    Update main.c

--- main.c
+++ main.c
@@ -12,12 +12,12 @@ int main(void)
 {
 	DDRA = 0xFF;
 	DDRB = 0x0F;
-	DDRC = 0x0F
+	DDRC = 0x0F;
 	unsigned char FND[10] = {0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F};
 	while (1) // 무한반복
 	{
 		PORTB=0xFF;
-		if((PINB & 0x01)!=0) //DIP스위치 1번의값이 0이 아니면
+		if((PINC & 0x10)!=0) //DIP스위치 1번의값이 0이 아니면
 		{  
 			for (int i = 0; i < 10; i++) //10번 반복
 				{
@@ -25,7 +25,7 @@ int main(void)
 				_delay_ms(500); //0.5초 딜레이
 				}
 		}
-		if((PINB & 0x02)!=0) //DIP스위치 2번의값이 0이 아니면
+		if((PINC & 0x20)!=0) //DIP스위치 2번의값이 0이 아니면
 		{
 			for (int i = 10; i >= 0; i--) //반대로 10번 반복
 				{
